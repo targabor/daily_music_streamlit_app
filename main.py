428b95938ea4c5978880e779962020a97e327cc9
albabuhin <129290710+albabuhin@users.noreply.github.com>
2023-04-03 23:50:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def email_is_valid(email: str):
     else:
       return False
 
-sl.write('If you want to get weekly updates of #daily-music and get to know a bunch of fun stats, subscribe to the email list!')
+sl.write('If you want to get weekly updates of #daily-music and get to know a bunch of fun stats, subscribe to the mailing list!')
 email = sl.text_input('', value='example@example.com')
 if sl.button('Subscribe'):
     con = snowflake.connector.connect(**sl.secrets["snowflake"])
