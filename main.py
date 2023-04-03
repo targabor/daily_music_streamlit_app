f9594270048a47e5f751a7e8894c39f37ef5dae6
Alba Nemeth-Buhin <buhina@starschema.com>
2023-04-03 17:07:02 +0200

    add subscribe button and placeholder for action

--- main.py
+++ main.py
@@ -2,5 +2,17 @@ import streamlit as sl
 
 sl.header('Schema Music coming soon...')
 
+def add_mail_to_mailing_list(mail: str):
+    # create snowflake connection
+    # create statement and add mail
+    # could add nickname to be more personal
+    sl.write(mail)
+
+def validate_mail(mail: str):
+    pass
+
 sl.write('If you want to get weekly updates of #daily-music and get to know a bunch of fun stats, subscribe to the email list!')
-sl.input('examle@example.com')
+mail = sl.text_input('email', value='examle@example.com')
+sl.button('Subscribe', on_click=add_mail_to_mailing_list, args=(mail))
+
+
