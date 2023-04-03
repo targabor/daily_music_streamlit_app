0f4da2c19267941d2fe91564918027ca86551f5d
Alba Nemeth-Buhin <buhina@starschema.com>
2023-04-03 16:54:25 +0200

    add input field

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as sl
 
-
 sl.header('Schema Music coming soon...')
+
+sl.write('If you want to get weekly updates of #daily-music and get to know a bunch of fun stats, subscribe to the email list!')
+sl.input('examle@example.com')
