695edfea305cc7615c1be7a8969d7b2d3d2a14fd
albabuhin <129290710+albabuhin@users.noreply.github.com>
2023-04-03 23:36:49 +0200

    add email list subscription function

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import streamlit as sl
+import requests
+import snowflake.connector
 
-sl.header('Schema Music coming soon...')
+sl.header('Schema Music')
 
-def add_mail_to_mailing_list(mail: str):
-    # create snowflake connection
-    # create statement and add mail
-    # could add nickname to be more personal
-    sl.write(mail)
+def add_email_to_mailing_list(email: str):
+    with con.cursor() as cur:
+        cur.execute(f"INSERT INTO CONSOLIDATED.subscribers VALUES ('{email}')")
+    sl.write(f'subscribed with {email}')
 
 def validate_mail(mail: str):
     pass
 
-sl.write('If you want to get weekly updates of #daily-music and get to know a bunch of fun stats, subscribe to the email list!')
 
-
-mail = sl.text_input('email', value='examle@example.com')
+email = sl.text_input('If you want to get weekly updates of #daily-music and get to know a bunch of fun stats, subscribe to the email list!', value='examle@example.com')
 if sl.button('Subscribe'):
-    add_mail_to_mailing_list(mail)
+    con = snowflake.connector.connect(**sl.secrets["snowflake"])
+    add_email_to_mailing_list(email)
 
