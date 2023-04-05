8e70dce3a6cb3ccecf64f51daa5e8c935994e023
Gábor Tar <gabor.tar@hcl.com>
2023-04-05 13:10:03 +0200

    addig hashed id to emails

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ import requests
 import snowflake.connector
 import re
 
-regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+regex = re.compile(
+    r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
 sl.header('Schema Music')
+
 
 def add_email_to_mailing_list(email: str):
     if email_is_valid(email) and email != 'example@example.com':
         with con.cursor() as cur:
-            cur.execute(f"INSERT INTO CONSOLIDATED.subscribers VALUES ('{email}')")
+            cur.execute(
+                "INSERT INTO CONSOLIDATED.subscribers VALUES (ABS(HASH(%s)),%s)", (email, email))
             sl.write(f'subscribed with {email}')
     else:
         sl.write('please add a valid email address')
-        
-    
+
 
 def email_is_valid(email: str):
     if re.fullmatch(regex, email):
-      return True
+        return True
     else:
-      return False
+        return False
+
 
 sl.write('If you want to get weekly updates of #daily-music and get to know a bunch of fun stats, subscribe to the mailing list!')
 email = sl.text_input('', value='example@example.com')
@@ -29,4 +32,3 @@ if sl.button('Subscribe'):
     con = snowflake.connector.connect(**sl.secrets["snowflake"])
     add_email_to_mailing_list(email)
     con.close()
-
