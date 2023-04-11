5e68c716e93b4150b42b276466fa9b5f464bfe55
Gábor Tar <gabor.tar@hcl.com>
2023-04-11 10:04:29 +0200

    secrets

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import pandas as pd
 
 sl.header('Schema Music')
 
+snowflake_secrets = sl.secrets['snowflake']
+
 # Interactive table based on Snowflake
-all_track_data = snowflake_functions.get_all_track_data()
+all_track_data = snowflake_functions.get_all_track_data(snowflake_secrets)
 all_track_data.set_index('Song Title', inplace=True)
 search_title = sl.text_input('Search by track name')
 if search_title:
